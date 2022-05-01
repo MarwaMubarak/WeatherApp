2168aa869d1eb1a4cec3146c50aa6c830f655d66
Marwa Mubarak <marwamubarak6@gmail.com>
2022-05-01 08:41:37 +0200

    Update root_app.dart

--- lib/src/root_app.dart
+++ lib/src/root_app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 import 'package:weather_app/blocs/city_weather_cubit.dart';
 import 'package:weather_app/models/city_weather_model.dart';
 
@@ -8,6 +7,8 @@ import '../views/home_view.dart';
 class RootApp extends StatelessWidget {
 
   CityWeatherModel? cityWeatherModel;
+
+  RootApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
