0231f20fd5e5ba5d064b55b4b47e33ef665a28fd
Marwa Mubarak <marwamubarak6@gmail.com>
2022-05-01 08:41:41 +0200

    Update weather_service.dart

--- lib/services/weather_service/weather_service.dart
+++ lib/services/weather_service/weather_service.dart
@@ -2,45 +2,40 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:weather_app/models/city_weather_model.dart';
-class CityWeatherService{
-  String baseUrl='http://www.metaweather.com';
 
-  Future<int?>getCityID({required String cityName})async
-  {
+class CityWeatherService {
+  String baseUrl = 'http://www.metaweather.com';
+
+  Future<int?> getCityID({required String cityName}) async {
     int? cityID;
-    try{
+    try {
       Uri url = Uri.parse('$baseUrl/api/location/search/?query=$cityName');
-     http.Response response = await http.get(url);
-     print(response.body);
-     List<dynamic> data = jsonDecode(response.body);
+      http.Response response = await http.get(url);
+      print(response.body);
+      List<dynamic> data = jsonDecode(response.body);
 
-     cityID= data[0]["woeid"];
-     print(cityID);
-   }catch(e){
-
-   }
-     return cityID;
+      cityID = data[0]["woeid"];
+      print(cityID);
+    } catch (e) {
+      print(e);
+    }
+    return cityID;
   }
- Future<CityWeatherModel?> getWeather({required String cityName }) async
-  {
+
+  Future<CityWeatherModel?> getWeather({required String cityName}) async {
     CityWeatherModel? cityWeatherData;
-   try{
-     int? id= await getCityID(cityName: cityName);
-     print('idddddddddddddddddddddd  $id');
-     Uri url=Uri.parse('$baseUrl/api/location/$id/');
-     http.Response response = await http.get(url);
-     print(response.body);
-     Map<String ,dynamic> jsonData = jsonDecode(response.body);
-     Map<String,dynamic> cityWeather =jsonData["consolidated_weather"][0];
-     cityWeatherData = CityWeatherModel.fromJson(cityWeather);
-
-   }
-   catch(e) {
-     print(e);
-   }
+    try {
+      int? id = await getCityID(cityName: cityName);
+      print('idddddddddddddddddddddd  $id');
+      Uri url = Uri.parse('$baseUrl/api/location/$id/');
+      http.Response response = await http.get(url);
+      print(response.body);
+      Map<String, dynamic> jsonData = jsonDecode(response.body);
+      Map<String, dynamic> cityWeather = jsonData["consolidated_weather"][0];
+      cityWeatherData = CityWeatherModel.fromJson(cityWeather);
+    } catch (e) {
+      print(e);
+    }
     return cityWeatherData;
-
-
-
   }
 }
