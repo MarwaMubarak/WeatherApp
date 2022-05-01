e04c5f7f4868536635c95fd858afd9b9a32002ec
Marwa Mubarak <marwamubarak6@gmail.com>
2022-05-01 08:17:04 +0200

    Create city_weather_state.dart

---
+++ lib/blocs/city_weather_state.dart
@@ -0,0 +1,12 @@
+part of 'city_weather_cubit.dart';
+
+@immutable
+abstract class CityWeatherState {}
+
+class CityWeatherInitial extends CityWeatherState {}
+class NavigationState extends CityWeatherState{}
+class AssignWeatherState extends CityWeatherState{}
+
+
+
+
